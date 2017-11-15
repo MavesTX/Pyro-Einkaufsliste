1e64ec38436aa2b4fc47d0d9dda1af40bdd9f800
MavesTX <33699916+MavesTX@users.noreply.github.com>
2017-11-15 21:09:57 +0100

    Add Timer, Add Button, Fix

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+/* =========== DAY / MONTH / YEAR =========== */
+
+$day 		= "28"; // Tage 1-31
+$month 		= "Dec"; // Monate: Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
+$year 		= "2017"; // Jahr 
+
+/* =========== HOUR / MINUTES / SECOUNDS =========== */
+
+$hour 		= "06"; // Stunden 	00 - 24
+$minutes 	= "00"; // Minuten 	00 - 60
+$secounds 	= "00"; // Sekunden 00 - 60
+
+$end_message = "Viel Spaß beim Einkaufen!";
+
+?>
--- index.php
+++ index.php
@@ -5,24 +5,47 @@
 	  <title>Silvester - Einkaufsliste</title>
 	  <meta charset="utf-8">
 	  <meta name="viewport" content="width=device-width, initial-scale=1">
-	  <link rel="stylesheet" href="css/style.css">
-	  <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
+	  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Play">
 	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+	  <link rel="stylesheet" href="/css/style.css">
 	  <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
 	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+	  <script>
+var countDownDate = new Date("<?= $month; ?> <?= $day; ?>, <?= $year; ?> <?= $hour; ?>:<?= $minutes; ?>:<?= $secounds; ?>").getTime();
+
+var x = setInterval(function() {
+
+    var now = new Date().getTime();
+    
+    var distance = countDownDate - now;
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("timer").innerHTML = days + " Tage | " + hours + " Std. | "
+    + minutes + " Min. | " + seconds + " Sek.";
+     
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "<?= $end_message ?>";
+    }
+}, 1000);
+</script>
 </head>
 <body>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
   <ul class="navbar-nav">
-  <li class="navbar-brand">Pyro - Einkaufsliste</li>
     <li class="nav-item">
+	  <li class="nav-link" id="timer">Timer loaded..</li>
     </li>
   </ul>
 </nav>
-
-<div class="container" style="margin-top: 100px;"> <!-- ToDo: Add stylesheet in style.css -->
+<center><a href="/add/" type="button" id="add_button" class="btn btn-primary">Artikel hinzufügen</a></center>
+<div class="container">
   <div id="accordion">
     <div class="card">
       <div class="card-header">
